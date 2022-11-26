f70a5570f59ab6f3293d849ca40e1b5cdcb9cbd1
Overload02 <alvajoy123@gmail.com>
2022-11-26 12:14:00 -0500

    Cleaned formatting.

--- notify/notify_core.h
+++ notify/notify_core.h
@@ -4,9 +4,9 @@
  * @brief Oxygen Notification system.
  * @version 0.1
  * @date 2022-08-11
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #ifndef NOTIFY_CORE_H
@@ -18,56 +18,64 @@
 #define NOTIFY_STACK_LIMIT 255
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif /* __cplusplus */
 
-/**
- * @brief Notifications structure file.  
- */ 
-struct notify_t{
-	// Simple notification information. 
-	gfx_sprite_t *icon;	
-	char title[9];
-	char text[30];
-};
-extern struct notify_t *notify;
+	/**
+	 * @brief Notifications stack structure
+	 */
+	struct notify_t
+	{
+		// Simple notification information.
+		gfx_sprite_t *icon;
+		char title[9];
+		char text[30];
+	};
+	extern struct notify_t *notify;
 
-/* Notification Stack amount */
-extern uint8_t notify_amount;
+	/**
+	 * @brief This holds the amount of notifications in the stack
+	 */
+	extern uint8_t notify_amount;
 
-/**
- * Creates a new notification.
- * @param icon Icon of the notification
- * @param title Title of the notification (Name of Program).
- * @param text Text or dialog of the notifications.
- */
-int notify_Create(gfx_sprite_t *icon, char title[9], char text[30]);
+	/**
+	 * Creates a new notification.
+	 * @param icon Icon of the notification
+	 * @param title Title of the notification (Name of Program).
+	 * @param text Text or dialog of the notifications.
+	 */
+	int notify_Create(gfx_sprite_t *icon, char title[9], char text[30]);
 
-/**
- * @brief Deletes Notification out of a stack,
- * @param index index in stack.
- */
-void notify_Delete(uint8_t index);
+	/**
+	 * @brief Deletes Notification out of a stack,
+	 * @param index index in stack.
+	 */
+	void notify_Delete(uint8_t index);
 
-/**
- * @brief This function deletes all the notification in notification stack.
- */ 
-void notify_DeleteAll(void);
+	/**
+	 * @brief This function deletes all the notification in notification stack.
+	 */
+	void notify_DeleteAll(void);
 
-/**
- * @brief Renders notification at the top of the notification stack.
- */ 
-int notify_Alert(void);
+	/**
+	 * @brief Renders notification at the top of the notification stack.
+	 */
+	int notify_Alert(void);
 
-/**
- * @brief Renders a notification.
- */ 
-void notify_Render(int x, int y, int index);
+	/**
+	 * @brief Renders a notification based on stack index
+	 * @note
+	 * @param  x: X position
+	 * @param  y: Y position
+	 * @param  index: Index where the notification is located in the stack
+	 */
+	void notify_Render(int x, int y, int index);
 
-/**
- * @brief Renders all the notification in a tray.
- */ 
-void notify_Tray(void);
+	/**
+	 * @brief Renders all the notification in a tray.
+	 */
+	void notify_Tray(void);
 
 #ifdef __cplusplus
 }
--- notify/notify_save.h
+++ notify/notify_save.h
@@ -1,39 +1,29 @@
-/**
- * @file notify_save.h
- * @author Alvajoy Asante
- * @brief Notifaction appvar saving.
- * @version 0.1
- * @date 2022-08-11
- * 
- * @copyright Copyright (c) 2022
- * 
- */
-
 #ifndef NOTIFY_SAVE_H
 #define NOTIFY_SAVE_H
 
-#define NOTIFY_APPVAR_NAME	"NOTIFY"
+#define NOTIFY_APPVAR_NAME "NOTIFY"
 
 #include <tice.h>
 #include <fileioc.h>
 #include <string.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif /* __cplusplus */
 
-/**
- * @brief Loads all the files from notify appvar
- */ 
-bool notify_Load(void);
+    /**
+     * @brief Loads notification data.
+     */
+    bool notify_Load(void);
 
-/**
- * @brief Saves notify sturct.
- */ 
-void notify_Save(void);
+    /**
+     * @brief Saves notification data.
+     */
+    void notify_Save(void);
 
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
 
-#endif /* __NOTIFY_SAVE_H__ */ 
+#endif
